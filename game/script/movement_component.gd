0c780c3ca3d022bf79a934b7150689285414c9cd
godotc <disyourself@qq.com>
2023-11-01 02:31:08 +0800

    mangae the rust binding, a start that not bad!

---
+++ game/script/movement_component.gd
@@ -0,0 +1,21 @@
+extends MovementComponent
+
+var character:CharacterBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	character= get_parent()
+	print(get_character_root())
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var move_vec = Vector2(character.velocity.x, character.velocity.z);
+	self.animation_tree.set("parameters/conditions/idle",
+		move_vec== Vector2.ZERO && character.is_on_floor());
+	self.animation_tree.set("parameters/conditions/walk", 
+		move_vec!= Vector2.ZERO && character.is_on_floor());
+	self.animation_tree.set("parameters/conditions/jump",
+		 ! character.is_on_floor() &&  Input.is_action_pressed("space"));
+
+	pass
--- rust/src/movement_component.rs
+++ rust/src/movement_component.rs
@@ -1,34 +1,35 @@
-use godot::builtin::GodotString;
-use godot::engine::{CharacterBody3D, InputEvent, Node};
+use godot::engine::{AnimationTree, CharacterBody2D, CharacterBody3D, Input, InputEvent, Node};
+use godot::prelude::utilities::move_toward;
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Node)]
 struct MovementComponent {
-    speed: f32,
-    jump: bool,
-
-    parent: Option<Gd<CharacterBody3D>>,
-
     #[base]
     node: Base<Node>,
+
+    #[export]
+    character_root: Option<Gd<CharacterBody3D>>,
+
+    #[export]
+    animation_tree: Option<Gd<AnimationTree>>,
+
+    last_lookat: Vector3,
+
+    #[export]
+    lookat_smoth_weight: real,
+
+    #[export]
+    speed: real,
+    #[export]
+    jump_velocity: real,
 }
 
 #[godot_api]
 impl MovementComponent {
     #[func]
-    fn hello_world(&self, who: GodotString) {
-        self.hello_internal(who);
-    }
-    #[func]
-    fn bello_world(&self, who: GodotString) {}
-
-    fn hello_internal(&self, who: GodotString) {
-        static I: i32 = 1;
-        let x = 2;
-        if I != x {
-            godot_print!("hello world! {} {}", I, who.to_string());
-        }
+    fn hello(&self) {
+        godot_print!("hello world");
     }
 }
 
@@ -36,19 +37,86 @@ impl MovementComponent {
 impl NodeVirtual for MovementComponent {
     fn init(node: Base<Node>) -> Self {
         godot_print!("The MovementComponent in rust init!");
-        let Component = Self {
-            speed: 0.0,
-            jump: false,
+
+        let mut component = Self {
             node,
-            parent: None,
+            character_root: None,
+            animation_tree: None,
+            last_lookat: Vector3::ZERO,
+            lookat_smoth_weight: 0.5,
+            jump_velocity: 4.5,
+            speed: 5.0,
         };
-        // Component.node.get_parent().unwrap();
-        Component
+
+        component.character_root = component
+            .node
+            .get_parent()
+            .and_then(|x| x.try_cast::<CharacterBody3D>());
+
+        // Specify by editor now....
+        // if component.parent.is_some() {
+        //     godot_print!(
+        //         "Found the parent {}",
+        //         component.parent.as_ref().unwrap().to_string()
+        //     );
+        // } else {
+        //     println!("Not found")
+        // }
+        // let machine = component.node.find_child("AnimationTree".into());
+        // if machine.is_some() {
+        //     component.animation_tree = machine.unwrap().try_cast::<AnimationTree>();
+        // }
+
+        component
     }
 
-    fn physics_process(&mut self, delta: f64) {
-        self.speed = self.speed.sin() * 200.0 * delta as f32 + 1.5;
-        // godot_print!("The new speed {}", self.speed);
+    fn process(&mut self, delta: f64) {
+        godot_print!("abacacdac");
+        if self.character_root.is_none() {
+            return;
+        }
+        let mut character = self.character_root.as_deref_mut().unwrap();
+
+        let input = Input::singleton();
+        let input_vec = input.get_vector("a".into(), "d".into(), "s".into(), "w".into());
+
+        let dir = Vector3::ONE
+            * Vector3 {
+                x: input_vec.x,
+                y: 0.0,
+                z: input_vec.y,
+            }
+            .normalized();
+
+        if dir != Vector3::ZERO {
+            let forward = -character.get_global_transform().basis.rows[2];
+
+            let lerped_lookat = Vector3::lerp(
+                self.last_lookat,
+                Vector3 {
+                    x: forward.x,
+                    y: character.get_global_position().y,
+                    z: forward.z,
+                },
+                self.lookat_smoth_weight,
+            );
+
+            character.look_at(lerped_lookat);
+
+            character.set_velocity(Vector3 {
+                x: dir.x * self.speed,
+                y: character.get_velocity().y,
+                z: dir.z * self.speed,
+            });
+        } else {
+            character.set_velocity(Vector3 {
+                x: move_toward(character.get_velocity().x.into(), 0.0, self.speed.into()) as f32,
+                y: character.get_velocity().y,
+                z: move_toward(character.get_velocity().z.into(), 0.0, self.speed.into()) as f32,
+            });
+        }
+
+        character.move_and_slide();
     }
 
     fn input(&mut self, event: Gd<InputEvent>) {}
